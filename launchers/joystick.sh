b4d3d78551cd13b8a7c8a3130c85edf41e4c2436
Liam Paull <paulll@iro.umontreal.ca>
2024-09-23 07:38:47 -0400

    Revert "simplified exercise to work with ente infra"
    This reverts commit 2561d7b01d676a3cc6af110596585b117a5fe9a7.

--- launchers/joystick.sh
+++ launchers/joystick.sh
@@ -5,5 +5,9 @@ source /environment.sh
 
 source /opt/ros/noetic/setup.bash
 source /code/catkin_ws/devel/setup.bash --extend
+source /code/submission_ws/devel/setup.bash --extend
 
-exec rosrun dt-joystick-demo dt-joystick-demo-node.py
+dt-exec-BG roslaunch --wait agent agent_node.launch
+dt-exec-FG rosrun dt-joystick-demo dt-joystick-demo-node.py
+
+copy-ros-logs
